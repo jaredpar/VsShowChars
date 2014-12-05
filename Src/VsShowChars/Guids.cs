eedc884dec471fb7ea90f1c48356ea0b6847a970
Jared Parsons <jaredpar@microsoft.com>
2014-12-04 17:28:14 -0800

    Initial project spit

---
+++ Src/VsShowChars/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace JaredPar.VsShowChars
+{
+    static class GuidList
+    {
+        public const string guidVsShowCharsPkgString = "b2e3f7e8-cf16-419f-bbe4-1abe95c4dd6a";
+        public const string guidVsShowCharsCmdSetString = "694a780c-9677-4c06-8f99-78347cb683ac";
+
+        public static readonly Guid guidVsShowCharsCmdSet = new Guid(guidVsShowCharsCmdSetString);
+    };
+}
